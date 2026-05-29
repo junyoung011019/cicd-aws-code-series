95953adbc1e439c23561728d04bd644ed2d17727
junyoung_1019 <jungjunyoung_megazone@amorepartners.com>
2026-05-29 17:47:48 +0900

    Java 설치 및 배포 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,10 +9,15 @@ mkdir -p /home/ec2-user/spring/logs
 # 3. 배포 시작 시간 기록
 echo "===== 배포 시작: $(TZ=Asia/Seoul date '+%Y-%m-%d %H:%M:%S') =====" >> $LOG_FILE
 
-# 4. 기존 프로세스 있다면 종료
+# 4. Java 설치 (없을 경우)
+if ! command -v java &> /dev/null; then
+    sudo yum install -y java-21-amazon-corretto
+fi
+
+# 5. 기존 프로세스 있다면 종료
 if pgrep -f '*.jar' > /dev/null; then
     kill -9 $(pgrep -f '*.jar')
 fi
 
-# 5. Spring 프로세스 기동
-nohup java -jar /home/ec2-user/spring/build/libs/*.jar >> $LOG_FILE 2>&1 &
+# 6. Spring 프로세스 기동
+nohup java -jar /home/ec2-user/spring/build/libs/*[^plain].jar >> $LOG_FILE 2>&1 &
